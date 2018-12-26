40ec8efc39922bc31a9c380729510ce35aefac20
rami642 <rami642@gmail.com>
2018-12-26 20:15:39 +0200

    Learning shell

---
+++ time.sh
@@ -0,0 +1,10 @@
+################################################
+# Created by : Rami642
+# Purpose : Learning Shell
+# Date : 26/12/2018
+# Version : 0.0.1
+################################################
+#Printing time before and after sleep
+date
+sleep 60
+date
