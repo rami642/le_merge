1ab1e29ca321aa3f31148d80f99ab669b5ebdc91
rami642 <rami642@gmail.com>
2018-12-30 18:56:14 +0200

    V2

--- scriptmpl.sh
+++ scriptmpl.sh
@@ -4,18 +4,18 @@
 # created by : rami642
 # purpose: template generation for script
 # date       : 30.12.18
-# version    : 0.0.1
+# version    : 0.0.2
 ###############################################
 
 #Vars
 #conf vars
-shabeng="/usr/bin/env"
+shabeng="/usr/bin/env bash"
 script_name="newscript.sh"
 extension="sh"
 Hash="#"
 Date="Date: $(date +%d/%m/%y)"
 Ver="Version : 0.0.0"
-Time=1
+Time="2"
 Purp="Purpose: "
 Auth="Created By : "
 line="#######################################"
@@ -23,23 +23,26 @@ line="#######################################"
 #msg vars
 msg_start="Started!"
 msg_end="FIN"
-msg_create="Creating Script Template"
+msg_create="Generating Script Template"
 
 ##############################################################################
 #This part is a msg that the template starting
 
 printf "%s \n" $line
-printf "%s \n" $msg_start
+echo $msg_start
 printf "%s \n" $line
 
 #this is a deley
 
+	sleep $Time
+
+printf "%s \n" $line
+echo $msg_create
+printf "%s \n" $line
 
 	sleep $Time
 
 #this is the template of the script
-
-printf "%s \n" $msg_create
 printf "%s \n" "#!$shabeng" > "$script_name\.$extension"
 printf "%s \n" $line >> "$script_name\.$extension"
 printf "%s \n" "$Hash $Auth" >> "$script_name\.$extension"
@@ -55,6 +58,10 @@ printf "%s \n" $line >> "$script_name\.$extension"
 #ending msg
 
 printf "%s \n" $line
-printf "%s \n" $msg_end
+echo $msg_end
 printf "%s \n" $line
 
+	sleep $Time
+
+#premissions and name change
+chmod +x ./scriptmpl.sh ; mv "$script_name\.$extension" newscript.sh ; vim newscript.sh 
