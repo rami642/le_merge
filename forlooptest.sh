a38ff7fd9ec2c1314aa20199c70a0948d35cfe11
rami642 <rami642@gmail.com>
2019-01-02 19:26:38 +0200

    for loop test

---
+++ forlooptest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#######################################
+# Created By : Rami642
+# Purpose: learning for loop
+# Date: 02/01/19
+# Version : 0.0.1
+#######################################
+for counter in {1..15}
+    do
+        echo counting from 1 to 15, not at $counter
+            sleep 1
+    done
