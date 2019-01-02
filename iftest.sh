059581b3033dbe379944237b9c08806046585562
rami642 <rami642@gmail.com>
2019-01-02 18:35:03 +0200

    if function testing

---
+++ iftest.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash 
+####################################### 
+# Created By :Rami642  
+# Purpose:  Learning IF
+# Date: 02/01/19 
+# Version : 0.0.1 
+#######################################
+if [ -z $1 ];then
+	echo "****rental Unknow****"
+elif [ -n $1 ];then
+	rental=$1
+fi
+#case parameters
+case $rental in
+	"car" | "CAR" | "Car") echo "for $rental rental is 100 nis" ;;
+	"van" |"Van") echo " for $rental rental is 500 nis " ;;
+	*) echo "Sorry no $rental found" ;;
+
+esac
